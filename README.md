5702965d46175c45e9d58f9e28003dc5159efa2e
kumarswamyk <68528859+kumarswamyk@users.noreply.github.com>
2022-12-15 17:17:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-8rd1k1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-8rd1k1)
---
+++ src/Components/Traffic.js
@@ -0,0 +1,15 @@
+const Traffic = (state, action) => {
+  if ('trafficlightId' in action) {
+    return state.map((trafficlight) => {
+      if (action.trafficlightId === trafficlight.id) {
+        return { ...trafficlight };
+      }
+
+      return trafficlight;
+    });
+  }
+
+  return state;
+};
+
+export default Traffic;
